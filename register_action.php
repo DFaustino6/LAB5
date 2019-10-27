1ba5ae98eda70b1f41ad18247546741b585274e5
DFaustino6 <a61171@ualg.pt>
2019-10-27 23:08:30 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -12,7 +12,7 @@
    			$nrows  = mysql_num_rows($result);
    			for($i=0; $i<$nrows; $i++)
      			$tuple[$i] = mysql_fetch_array($result,MYSQL_ASSOC);
-			if($query<0)
+			if($query>0)
 				return 1;
 			if($_REQUEST['ConfPwd']!=$_REQUEST['Pwd'])
 				return 4;
