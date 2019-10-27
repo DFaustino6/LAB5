0ce3436423e8e4915a5c6cab86c8cc6fc5e2e776
DFaustino6 <a61171@ualg.pt>
2019-10-27 21:45:44 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -1,12 +1,23 @@
 <?php
-	$query= "SELECT * from users where email = $_REQUEST['Email']"
-	if($_REQUEST['Email']>0)
-		return 1;
-	if($_REQUEST['ConfPwd']!=$_REQUEST['Pwd'])
-		return 4;
-	else{
-		$pwdHash=substr(md5($_REQUEST['Pwd']),0,32);
-		INSERT INTO users(name,email,password_digest,created_at,updated_at)
-		VALUES ($_REQUEST['Username'],$_REQUEST['Email'],$_REQUEST[$pwdHash],NOW(),NOW())
+	$db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
+
+	function checkSubmission(){
+	if($db){
+		$query= "SELECT * from users where email = $_REQUEST['Email']";
+	
+		if($_REQUEST['Email']>0)
+			return 1;
+		if($_REQUEST['ConfPwd']!=$_REQUEST['Pwd'])
+			return 4;
+		else
+			submit();
+		}
 	}
+
+	function submit(){
+		$pwdHash=substr(md5($_REQUEST['Pwd']),0,32);
+		$query="INSERT INTO users(name,email,password_digest,created_at,updated_at)
+		VALUES ($_REQUEST['Username'],$_REQUEST['Email'],$pwdHash,NOW(),NOW())";
+	}
+
 ?>	
