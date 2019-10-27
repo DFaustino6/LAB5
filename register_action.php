4a6abcb230d02555ee5f6a7235d83fa1a75d6c88
DFaustino6 <a61171@ualg.pt>
2019-10-27 23:56:05 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -18,16 +18,13 @@
 			else
 				submit($db);
 		}
-	
-	$query="INSERT INTO users(id,name,email,password_digest,created_at,updated_at)
-			VALUES ('12','test','test@test','sd',NOW(),NOW())";
 
 	function submit($db){
 			$pwdHash=substr(md5($_REQUEST['Pwd']),0,32);
 			$Email = $_REQUEST['Email'];
 			$Username = $_REQUEST['Username'];
 			$query="INSERT INTO users(name,email,password_digest,created_at,updated_at)
-			VALUES ($Username,$Email,$pwdHash,NOW(),NOW())";
+			VALUES ('$Username','$Email','$pwdHash',NOW(),NOW())";
 	}
 	mysql_close($db);
 ?>	
