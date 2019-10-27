720fa0ca2811552766866a2fb203c9d8e3c6f75b
DFaustino6 <a61171@ualg.pt>
2019-10-27 22:07:17 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -2,27 +2,28 @@
 	include 'db.php';
 	$db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 
-	function checkSubmission(){
-	if($db){
-		$query= "SELECT * from users where email = {$_REQUEST['Email']}";
-	
-		if($_REQUEST['Email']>0)
-			return 1;
-		if($_REQUEST['ConfPwd']!=$_REQUEST['Pwd'])
-			return 4;
-		else
-			submit();
+	function checkSubmission($db){
+		if($db){
+			$query= "SELECT * from users where email = {$_REQUEST['Email']}";
+		
+			if($_REQUEST['Email']>0)
+				return 1;
+			if($_REQUEST['ConfPwd']!=$_REQUEST['Pwd'])
+				return 4;
+			else
+				submit();
 		}
+	 mysql_close($db);
 	}
 
-	function submit(){
-	if ($db) {
-		echo "entered submit";
-		$pwdHash=substr(md5($_REQUEST['Pwd']),0,32);
-		$query="INSERT INTO users(name,email,password_digest,created_at,updated_at)
-		VALUES ({$_REQUEST['Username']},{$_REQUEST['Email']},{$pwdHash},NOW(),NOW())";
+	function submit($db){
+		if ($db) {
+			echo "entered submit";
+			$pwdHash=substr(md5($_REQUEST['Pwd']),0,32);
+			$query="INSERT INTO users(name,email,password_digest,created_at,updated_at)
+			VALUES ({$_REQUEST['Username']},{$_REQUEST['Email']},{$pwdHash},NOW(),NOW())";
+		}
+		 mysql_close($db);	
 	}
-		
-	}
-	 mysql_close($db);
+	
 ?>	
