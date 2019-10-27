d09f95950e03fa07bbbdb31c721a36893c17b5b1
DFaustino6 <a61171@ualg.pt>
2019-10-27 23:28:25 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -6,6 +6,7 @@
 	$db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 		if($db && !empty($_REQUEST)){
 			$Email = $_REQUEST['Email'];
+			echo $Email;
 			$query= "SELECT * from users where email = $Email";
 			if(!($result = @ mysql_query($query,$db)))
    				showerror();
@@ -25,7 +26,6 @@
 			$pwdHash=substr(md5($_REQUEST['Pwd']),0,32);
 			$Email = $_REQUEST['Email'];
 			$Username = $_REQUEST['Username'];
-			echo $Email." ".$Username;
 			$query="INSERT INTO users(name,email,password_digest,created_at,updated_at)
 			VALUES ($Username,$Email,$pwdHash,NOW(),NOW())";
 				
