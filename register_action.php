f407ce4c08cfcb227df4c802c0399448751167ce
DFaustino6 <a61171@ualg.pt>
2019-10-29 14:35:10 +0000

    Update register_action.php

--- register_action.php
+++ register_action.php
@@ -3,23 +3,37 @@
 	
 	$db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 		if($db && !empty($_REQUEST)){
-			$Email = $_REQUEST['Email'];
-			$query= "SELECT * from users where email = '$Email'";
-			if(!($result = @ mysql_query($query,$db)))
-   				showerror();
-   			$nrows  = mysql_num_rows($result);
-			if($nrows>0){
+			
+			if(existingEmail($db)){
 				$ErrorType=1;
-				print_r($ErrorType);
 				returnRegister("",$_REQUEST['Username'],$ErrorType);
 			}
-			elseif($_REQUEST['ConfPwd']!=$_REQUEST['Pwd']){
+			elseif(!samePwd()){
 				$ErrorType=4;
 				returnRegister($Email,$_REQUEST['Username'],$ErrorType);
 			}
 			else
 				submit($db);
 		}
+
+	function existingEmail($db){
+		$Email = $_REQUEST['Email'];
+		$query= "SELECT * from users where email = '$Email'";
+		if(!($result = @ mysql_query($query,$db)))
+   			showerror();
+   		$nrows  = mysql_num_rows($result);
+   		if($nrows>0){
+				return true;
+			}
+		else
+			return 0;
+	}
+
+	function samePwd(){
+		if($_REQUEST['ConfPwd']!=$_REQUEST['Pwd'])
+			return true;
+		return false;
+	}
 
 	function submit($db){
 			$pwdHash=substr(md5($_REQUEST['Pwd']),0,32);
